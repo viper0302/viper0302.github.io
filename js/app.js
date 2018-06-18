54fd1bad8d43bafaabf73a77ca49761d235e7a89
viper0302 <justin.j0302@gmail.com>
2018-06-17 21:39:49 -0600

    Added more functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 // Declare card symbols
-let cards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+let cardDeck = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
 
 
 /*
@@ -18,12 +18,12 @@ let cards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
 
 
 // Create array to hold opened cards and variables
-let openCard = [];
+let openedCards = [];
 let moves = 0;
 let starts = 3;
 let matchFound = 0;
 let startGame = false;
-let starRating = "3";
+let starRate = "3";
 let timer;
 
 // Shuffle cards (function from http://stackoverflow.com/a/2450976)
@@ -41,36 +41,36 @@ function shuffle(array) {
 }
 
 // Created each card's HTMl
-function createCard() {
-    let cardList = shuffle(cards);
+function createCardDecks() {
+    let cardList = shuffle(cardDeck);
     cardList.forEach(function(card) {
         $(".deck").append('<li><i class="card fa ' + card + '"></i></li>');
     })
 }
 
 // My own Logic to find matching cards
-function findMatch() {
+function matchingCards() {
     // Show cards on click
     $(".card").on("click", function() {
         if ($(this).hasClass("open show")) { return; }
         $(this).toggleClass("flipInY open show");
-        openCard.push($(this));
+        openedCards.push($(this));
         startGame = true;
-        // Check if classlist matches when openCard length == 2
-        if (openCard.length === 2) {
-            if (openCard[0][0].classList[2] === openCard[1][0].classList[2]) {
-                openCard[0][0].classList.add("bounceIn", "match");
-                openCard[1][0].classList.add("bounceIn", "match");
-                $(openCard[0]).off('click');
-                $(openCard[1]).off('click');
+        // Check if classlist matches when openedCards length == 2
+        if (openedCards.length === 2) {
+            if (openedCards[0][0].classList[2] === openedCards[1][0].classList[2]) {
+                openedCards[0][0].classList.add("bounceIn", "match");
+                openedCards[1][0].classList.add("bounceIn", "match");
+                $(openedCards[0]).off('click');
+                $(openedCards[1]).off('click');
                 matchFound += 1;
                 moves++;
                 removeOpenCards();
-                findWinner();
+                Winner();
             } else {
                 // If classes don't match, add "wrong" class
-                openCard[0][0].classList.add("shake", "wrong");
-                openCard[1][0].classList.add("shake", "wrong");
+                openedCards[0][0].classList.add("shake", "wrong");
+                openedCards[1][0].classList.add("shake", "wrong");
                 // Set timeout to remove "show" and "open" class
                 setTimeout(removeClasses, 1100);
                 // Reset openCard.length to 0
@@ -78,12 +78,12 @@ function findMatch() {
                 moves++;
             }
         }
-        updateMoves();
+        movesUpdater();
     })
 }
 
 // Update HTML with number of moves
-function updateMoves() {
+function movesUpdater() {
     if (moves === 1) {
         $("#movesText").text(" Move");
     } else {
@@ -92,19 +92,19 @@ function updateMoves() {
     $("#moves").text(moves.toString());
 
     if (moves > 0 && moves < 16) {
-        starRating = starRating;
+        starRate = starRate;
     } else if (moves >= 16 && moves <= 20) {
         $("#starOne").removeClass("fa-star");
-        starRating = "2";
+        starRate = "2";
     } else if (moves > 20) {
         $("#starTwo").removeClass("fa-star");
-        starRating = "1";
+        starRate = "1";
     }
 }
 
 
 // Open the  popup window when game is complete source: www.w3schools.com
-function findWinner() {
+function Winner() {
 
     if (matchFound === 8) {
 
@@ -112,7 +112,7 @@ function findWinner() {
         var span = document.getElementsByClassName("close")[0];
 
         $("#total-moves").text(moves);
-        $("#total-stars").text(starRating);
+        $("#total-stars").text(starRate);
 
         modal.style.display = "block";
 
@@ -133,7 +133,7 @@ function findWinner() {
 
 // function to Reset openCard.length to 0
 function removeOpenCards() {
-    openCard = [];
+    openedCards = [];
 }
 
 // Remove all classes unless they are a match
@@ -144,7 +144,7 @@ function removeClasses() {
 
 //  Used to disable clicks after a matched card
 function disableClick() {
-    openCard.forEach(function (card) {
+    openedCards.forEach(function (card) {
         card.off("click");
     })
 }
@@ -166,9 +166,9 @@ function startTimer() {
 }
 
 // Call functions
-shuffle(cards);
-createCard();
-findMatch();
+shuffle(cardDeck);
+createCardDecks();
+matchingCards();
 startTimer();
 
 // Function to restart the game on icon click
